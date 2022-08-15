36901ca153c02e906463f6412ece84b23b2883f3
Caio Martins <dev.caiomartins@gmail.com>
2022-08-15 13:58:25 -0300

    fade animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,20 @@ module.exports = {
           300: '#666',
         },
       },
+      keyframes: {
+        fadeInToRight: {
+          '0%': { opacity: '0', transform: 'translateX(-100%)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        fadeInToBottom: {
+          '0%': { opacity: '0', transform: 'translateY(-100%)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadeInToRight: 'fadeInToRight 2s',
+        fadeInToBottom: 'fadeInToBottom 0.8s',
+      },
     },
   },
   plugins: [],
